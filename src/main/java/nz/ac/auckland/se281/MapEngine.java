b4b1d4695bed4dd84d0f841249266a08b643d7f9
Tanush <tpan459@aucklanduni.ac.nz>
2025-06-03 02:42:01 +1200

    fixed casing error in show info country which allowed it to pass test case 3 of task 1

--- src/main/java/nz/ac/auckland/se281/MapEngine.java
+++ src/main/java/nz/ac/auckland/se281/MapEngine.java
@@ -46,14 +46,13 @@ public class MapEngine {
     while (true) {
       MessageCli.INSERT_COUNTRY.printMessage();
       String countryInput = Utils.scanner.nextLine().trim();
-      // Capitalize each word
       String[] words = countryInput.split("\\s+");
       StringBuilder sb = new StringBuilder();
       for (String word : words) {
         if (word.length() > 0) {
           sb.append(Character.toUpperCase(word.charAt(0)));
           if (word.length() > 1) {
-            sb.append(word.substring(1).toLowerCase());
+            sb.append(word.substring(1)); // keep rest as user input
           }
           sb.append(" ");
         }
